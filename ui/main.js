03102dfa8701ffc11748f73be4ea7e86cd553b49
ask5898 <alishahbaazk@gmail.com>
2017-02-11 23:47:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,7 @@ console.log('Loaded!');
 var element=document.getElementById("text");
 element.innerHTML="KISS MY ASS";
 
+//move the image
 var img=getElementById("img");
 img.onclick = function() {
  img.style.marginLeft="100px";
